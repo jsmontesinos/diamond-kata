dd4cf709d9f490639e787d32d32b1cd0b33a4462
Juan Sánchez <jsmontesinos@gmail.com>
2017-11-17 21:47:47 +0100

    First commit

---
+++ __tests__/diamond.spec.ts
@@ -0,0 +1,15 @@
+import diamond from '../src/diamond';
+
+describe('diamond kata', () => {
+  
+  // Act before assertions
+  beforeAll(async () => {
+    
+  });;
+
+  // Assert greeter result
+  it('If the input is null, return empty diamond', () => {
+    
+  });
+
+});
