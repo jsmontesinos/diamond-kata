45548d0620b50cc2d2268435c42b3302b981462c
Juan Sánchez <jsmontesinos@gmail.com>
2017-11-17 22:49:49 +0100

    Starting with B diamond

--- __tests__/diamond.spec.ts
+++ __tests__/diamond.spec.ts
@@ -1,15 +1,38 @@
+import "jest";
 import diamond from '../src/diamond';
 
 describe('diamond kata', () => {
   
-  // Act before assertions
-  beforeAll(async () => {
+  it('should return empty if input is null', () => {
+    const input = null;
     
-  });;
+    const output = diamond.print(input);
 
-  // Assert greeter result
-  it('If the input is null, return empty diamond', () => {
+    expect(output).toBe("");
+  });
+
+  it('should return empty if input is empty', () => {
+    const input = "";
     
+    const output = diamond.print(input);
+
+    expect(output).toBe("");
+  });
+
+  it('should return A if input is A', () => {
+    const input = "A";
+    
+    const output = diamond.print(input);
+
+    expect(output).toBe("A");
+  });
+
+  it('should return B diamond if input is B', () => {
+    const input = "B";
+    
+    const output = diamond.print(input);
+
+    expect(output).toBe(" A \nB B\n A ");
   });
 
 });
---
+++ src/diamond.ts
@@ -0,0 +1,15 @@
+class diamond {  
+  
+  public static print(arg0: string): string {
+    if (!arg0) {
+      return "";
+    }
+    else {
+      return "A";
+    }
+  }
+
+};
+
+export default diamond;
+
